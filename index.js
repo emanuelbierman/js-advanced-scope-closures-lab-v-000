65ff9485670090eefe09a954df6ecfdf01549702
emanuelbierman <emanuel.bierman@gmail.com>
2019-01-31 15:10:48 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function produceDrivingRange(blockRange) {
+  return function(start, end) {
+    const distance = Number(end.slice(0,2)) - Number(start.slice(0,2));
+    if (distance <= blockRange) {
+      return `within range by ${blockRange - distance}`;
+    } else {
+      return `${distance - blockRange} blocks out of range`;
+    }
+  };
+};
+
+function produceTipCalculator(percentage) {
+  return function(bill) {
+    return percentage * bill;
+  };
+};
+
+function createDriver() {
+  let driverId = 0;
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++driverId;
+    };
+  };
+};
