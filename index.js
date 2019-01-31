c1b6fd664273392e39ed57ccb70edae76878d6b0
emanuelbierman <emanuel.bierman@gmail.com>
2019-01-31 15:12:30 -0500

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ function produceDrivingRange(blockRange) {
     } else {
       return `${distance - blockRange} blocks out of range`;
     }
+
+    // let start = parseInt( startingBlock );
+    //  let end = parseInt( endingBlock );
+    //  let distanceToTravel = Math.abs( end - start );
+    //  let difference = blockRange - distanceToTravel;
+    //
+    //  if ( difference > 0 ) {
+    //    return `within range by ${difference}`
+    //  } else {
+    //    return `${Math.abs(difference)} blocks out of range`
+    //  }
   };
 };
 
